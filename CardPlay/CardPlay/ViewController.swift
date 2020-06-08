09d3ce209e248d51067643a98b7c29ac38a97281
rishikant kumar <rishi@rishikants-MacBook-Pro.local>
2020-06-09 00:28:08 +0530

    initial commit

---
+++ CardPlay/CardPlay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardPlay
+//
+//  Created by rishikant kumar on 08/06/20.
+//  Copyright © 2020 rishikant kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Silly Song/Silly Song/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Silly Song
+//
+//  Created by rishikant kumar on 06/06/20.
+//  Copyright © 2020 rishikant kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
